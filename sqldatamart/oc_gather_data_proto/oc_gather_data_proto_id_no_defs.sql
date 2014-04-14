ac5c40bd737a88243bda2ecac00f5c410fc51a54
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2014-04-14 18:25:31 +1000

    add cte for response labels add joins for retrieving oc_oids

--- sqldatamart/oc_gather_data_proto/oc_gather_data_proto_id_no_defs.sql
+++ sqldatamart/oc_gather_data_proto/oc_gather_data_proto_id_no_defs.sql
@@ -1,5 +1,4 @@
-﻿-- runtime on 2014-03-13 with 299423 rows in 1530ms
--- runtime on 2014-04-14 with 167854 rows in 1295ms
+﻿-- runtime on 2014-04-14 with 167854 rows in 6271ms
 
 CREATE INDEX i_item_group_metadata_itemid_crfversionid
   ON item_group_metadata
@@ -14,41 +13,98 @@ CREATE INDEX i_item_data_eventcrfid_itemid_status_live_notblank
    AND status_id <> 7 
    AND value::text <> ''::text;
 
+WITH parents AS (
+SELECT 
+  study.study_id
+, study.name
+FROM study
+WHERE 
+    study.status_id <> 5
+AND study.status_id <> 7
+)
+,
+ response_label AS (
+SELECT DISTINCT ON (rs_split.version_id, rs_split.label, rs_split.option_value) 
+  rs_split.version_id
+, rs_split.response_set_id
+, rs_split.label
+, rs_split.option_value
+, replace (rs_split.option_value_label, '###' , ',') option_value_label
+	FROM ( 
+	SELECT 
+	  rs_clean.version_id
+	, rs_clean.response_set_id
+	, rs_clean.label 
+	, trim (both FROM regexp_split_to_table(rs_clean.options_values, E',')) as option_value
+	, trim (both FROM regexp_split_to_table(rs_clean.options_text_clean, E',')) as option_value_label
+		FROM ( 
+		SELECT 
+		  response_type_id
+		, version_id
+		, response_set_id
+		, options_values
+		, response_set.label
+		, replace ( options_text, E'\\,' , '###') as options_text_clean
+		FROM response_set 
+		) rs_clean
+		where rs_clean.response_type_id IN (3,5,6,7)
+	) as rs_split)
+
 SELECT
-  COALESCE (parents.study_id,study.study_id,NULL) AS parent_study_id
-, ss.study_id
-, ss.study_subject_id
-, se.study_event_id
-, ec.event_crf_id
-, igm.item_group_id
-, id.item_data_id 
+  COALESCE (parents.name,study.name,NULL) AS study_name
+, study.name AS site_name
+, ss.label AS subject_id
+, sed.oc_oid AS event_oid
+, sed.ordinal AS event_order
+, se.sample_ordinal AS event_repeat
+, crfv.oc_oid AS crf_version_oid
+, ig.oc_oid AS item_group_oid
+, id.ordinal AS item_group_repeat
+, item.oc_oid AS item_oid
+, id.value AS item_value
+, rs.option_value_label AS item_value_label
 
-FROM item_data as id
+FROM item_data AS id
 
-INNER JOIN event_crf as ec
-on ec.event_crf_id=id.event_crf_id
+INNER JOIN event_crf AS ec
+ON ec.event_crf_id=id.event_crf_id
 
-INNER JOIN item_group_metadata as igm
-ON igm.item_id=id.item_id
+INNER JOIN crf_version AS crfv
+ON crfv.crf_version_id=ec.crf_version_id
+
+INNER JOIN item
+ON item.item_id=id.item_id
+
+INNER JOIN item_group_metadata AS igm
+ON igm.item_id=item.item_id
 AND igm.crf_version_id=ec.crf_version_id
 
-INNER JOIN study_event as se
-on se.study_event_id=ec.study_event_id
+INNER JOIN item_form_metadata AS ifm
+ON ifm.item_id=item.item_id
+AND ifm.crf_version_id=ec.crf_version_id
 
-INNER JOIN study_subject as ss
+INNER JOIN item_group AS ig
+ON ig.item_group_id=igm.item_group_id
+
+INNER JOIN study_event AS se
+ON se.study_event_id=ec.study_event_id
+
+INNER JOIN study_event_definition AS sed
+ON sed.study_event_definition_id=se.study_event_definition_id
+
+INNER JOIN study_subject AS ss
 on ss.study_subject_id=ec.study_subject_id
 
 INNER JOIN study
 on study.study_id=ss.study_id
 
-LEFT JOIN (
-SELECT study.study_id
-FROM study
-WHERE 
-    study.status_id <> 5
-AND study.status_id <> 7
-) AS parents
+LEFT JOIN parents
 ON parents.study_id=study.parent_study_id
+
+LEFT JOIN response_label AS rs
+ON rs.response_set_id=ifm.response_set_id
+AND rs.version_id=ifm.crf_version_id
+AND id.value=rs.option_value
 
 WHERE 
     study.status_id <> 5
