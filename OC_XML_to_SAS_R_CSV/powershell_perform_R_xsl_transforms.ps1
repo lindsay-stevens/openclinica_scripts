8ce9fd4f4bd90af658176101b8fa15e74fba5ce1
linassilva <linassilva@optusnet.com.au>
2014-06-04 14:39:26 +1000

    Changes to handle unicode characters
    R doesn't appear to handle unicode characters well.  The changes here are to convert unicode characters into the escaped unicode equivalent (\unnn).

--- OC_XML_to_SAS_R_CSV/powershell_perform_R_xsl_transforms.ps1
+++ OC_XML_to_SAS_R_CSV/powershell_perform_R_xsl_transforms.ps1
@@ -1,6 +1,50 @@
-param([string[]]$ocextract, [string[]]$dataframesxsl, [string[]]$dataframesout, [string[]]$dataframefactorsxsl, [string[]]$dataframefactorsout)
+param([string[]]$ocextract, [string[]]$projectpath, [string[]]$studyname)
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
-$xslt.Load("$dataframesxsl");
-$xslt.Transform("$ocextract", "$dataframesout");
-$xslt.Load("$dataframefactorsxsl");
-$xslt.Transform("$ocextract", "$dataframefactorsout");
+$xslt.Load("$projectpath" + "XmlWorkFiles\xml_convert_r_dataframes.xsl");
+$xslt.Transform("$ocextract", "$projectpath" + "XmlWorkFiles\xml_convert_r_dataframes_out.txt");
+$xslt.Load("$projectpath" + "XmlWorkFiles\xml_convert_r_dataframe_factors.xsl");
+$xslt.Transform("$ocextract", "$projectpath" + "XmlWorkFiles\xml_convert_r_dataframe_factors_out.txt");
+
+$filecontents = (get-content ("$projectpath" + "XmlWorkFiles\xml_convert_r_dataframes_out.txt"))
+$numlines = $filecontents.length
+for($i=0;$i -lt $numlines;$i++)
+{
+    $linelength = $filecontents[$i].length
+    for ($j=$linelength - 1;$j -ge 0;$j--){
+
+        if ([INT][CHAR]$filecontents[$i].substring($j,1) -gt 255) {
+	    $hexval = "0000" + ([convert]::tostring(([INT][CHAR]$filecontents[$i].substring($j,1) ),16))
+            $hexval = $hexval.substring($hexval.length - 4,4)
+            if ($j -eq $linelength - 1) {
+                $filecontents[$i] = $filecontents[$i].substring(0,$j) + "\u" + $hexval
+            }
+            else {
+                $filecontents[$i] = $filecontents[$i].substring(0,$j) + "\u" + $hexval + $filecontents[$i].substring(($j + 1),($filecontents[$i].length - ($j + 1)))
+            }
+        }
+    }
+}
+$filecontents | out-file ("$projectpath" + "R_"+ "$studyname"+"_DataFrames.R") -encoding ASCII
+
+
+$filecontents = (get-content ("$projectpath" + "XmlWorkFiles\xml_convert_r_dataframe_factors_out.txt"))
+$numlines = $filecontents.length
+for($i=0;$i -lt $numlines;$i++)
+{
+    $linelength = $filecontents[$i].length
+    for ($j=$linelength - 1;$j -ge 0;$j--){
+
+        if ([INT][CHAR]$filecontents[$i].substring($j,1) -gt 255) {
+	    $hexval = "0000" + ([convert]::tostring(([INT][CHAR]$filecontents[$i].substring($j,1) ),16))
+            $hexval = $hexval.substring($hexval.length - 4,4)
+            if ($j -eq $linelength - 1) {
+                $filecontents[$i] = $filecontents[$i].substring(0,$j) + "\u" + $hexval 
+            }
+            else {
+                $filecontents[$i] = $filecontents[$i].substring(0,$j) + "\u" + $hexval + $filecontents[$i].substring(($j + 1),($filecontents[$i].length - ($j + 1)))
+            }
+        }
+    }
+}
+$filecontents | out-file ("$projectpath" + "R_"+ "$studyname"+"_DataFrame_Factors.R") -encoding ASCII
+
