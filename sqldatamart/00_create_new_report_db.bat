1c25c996caa379a68b54474be6e499531b842e1f
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2014-06-27 11:19:10 +1000

    tidy psql version number

--- sqldatamart/00_create_new_report_db.bat
+++ sqldatamart/00_create_new_report_db.bat
@@ -22,14 +22,14 @@ set "SCRIPT08=%SCRIPTDIR%08_create_matviews_for_study_itemgroups.sql"
 set "SCRIPT09=%SCRIPTDIR%09_create_group_role_for_each_study.sql"
 set "SCRIPT10=%SCRIPTDIR%10_grant_usage_select_on_study_schema_tables_to_group_role.sql"
 
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d postgres -c "CREATE DATABASE %NEWDBNAME%;" -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT01% -v FDWSERVERNAME=%FDWSERVERNAME% -v FDWSERVERHOST=^'%FDWSERVERHOST%^' -v FDWSERVERPORT=^'%FDWSERVERPORT%^' -v FDWSERVERDBNAME=^'%FDWSERVERDBNAME%^' -v FDWSERVERUSER=^'%FDWSERVERUSER%^' -v FDWSERVERPASS=^'%FDWSERVERPASS%^' -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT02% -v FDWSERVERNAME=%FDWSERVERNAME% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT03% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT04% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT05% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT06% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT07% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT08% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT09% -P pager
-C:\"Program Files"\PostgreSQL\9.2\bin\psql -q -d %NEWDBNAME% -f %SCRIPT10% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d postgres -c "CREATE DATABASE %NEWDBNAME%;" -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT01% -v FDWSERVERNAME=%FDWSERVERNAME% -v FDWSERVERHOST=^'%FDWSERVERHOST%^' -v FDWSERVERPORT=^'%FDWSERVERPORT%^' -v FDWSERVERDBNAME=^'%FDWSERVERDBNAME%^' -v FDWSERVERUSER=^'%FDWSERVERUSER%^' -v FDWSERVERPASS=^'%FDWSERVERPASS%^' -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT02% -v FDWSERVERNAME=%FDWSERVERNAME% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT03% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT04% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT05% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT06% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT07% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT08% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT09% -P pager
+C:\"Program Files"\PostgreSQL\9.3\bin\psql -q -d %NEWDBNAME% -f %SCRIPT10% -P pager
