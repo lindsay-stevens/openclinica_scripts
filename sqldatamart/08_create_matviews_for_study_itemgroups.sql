54e17cfe5a3b9be9413cb2b2e0824255315a89bb
lindsay-stevens-kirby <lstevens@kirby.unsw.edu.au>
2014-08-06 12:32:44 +1000

    add more item_name handling

--- sqldatamart/08_create_matviews_for_study_itemgroups.sql
+++ sqldatamart/08_create_matviews_for_study_itemgroups.sql
@@ -75,9 +75,10 @@ FROM (
     study_name
    ,item_group_oid
    ,item_oid
-   -- if the item_name_hint is too long (64 or more) then trim it down
+   -- if the item_name_hint is too long (57 or more, allowing for 6 characters
+   -- added with '_label' ) then trim it down.
    -- this might happen due to prepending 'i_' for items starting with an int
-   ,CASE WHEN length(item_name_hint)>=64 THEN substr(item_name_hint,1,63)
+   ,CASE WHEN length(item_name_hint)>=57 THEN substr(item_name_hint,1,57)
        ELSE item_name_hint END as item_name_hint
    ,item_data_type
    ,item_form_order
@@ -88,7 +89,8 @@ SELECT DISTINCT study_name
       ,item_group_oid
       ,item_oid
       -- if the item_name starts with an integer, prepend 'i_'
-      ,(CASE WHEN item_name ~ '^[0-9]+$' THEN $$i_$$ || lower(substr(item_name,1,12))
+      , CASE WHEN length(item_name)>8 THEN item_oid ELSE
+        (CASE WHEN item_name ~ '^[0-9]+$' THEN $$i_$$ || lower(substr(item_name,1,12))
         ELSE lower(substr(item_name,1,12)) END
         || $$_$$
         ||  substr(
@@ -98,7 +100,7 @@ SELECT DISTINCT study_name
                     ,$$[\s]+$$, '_', 'g') -- change space(s) to underscore
                 ,$$[^\w]$$,'','g') -- remove non alphanum non underscore
             ,1,45)
-        ) as item_name_hint
+        ) END as item_name_hint
       ,item_data_type
       -- use max since item_form_order may change between crf versions
       ,max(item_form_order) AS item_form_order
